f8892ce1a73a77b503604adf9f4437b574a96e92
Tun1n <slpedro21@outlook.com>
2025-08-28 19:39:11 -0300

    Migrations PopularCategorias and PopularBebidas

---
+++ Solution1/WebApplication1/Migrations/20250828222641_InitialMigration.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    CategoriaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoriaName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.CategoriaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bebidas",
+                columns: table => new
+                {
+                    BebidaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    DescricaoCurta = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: false),
+                    DescricaoDetalhada = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: false),
+                    Preco = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    ImagemUrl = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: false),
+                    ImagemThumbnailUrl = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: false),
+                    IsBebidaPreferida = table.Column<bool>(type: "bit", nullable: false),
+                    EmEstoque = table.Column<bool>(type: "bit", nullable: false),
+                    CategoriaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bebidas", x => x.BebidaId);
+                    table.ForeignKey(
+                        name: "FK_Bebidas_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "CategoriaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bebidas_CategoriaId",
+                table: "Bebidas",
+                column: "CategoriaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bebidas");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
---
+++ Solution1/WebApplication1/Migrations/20250828222901_PopularCategorias.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class PopularCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias (CategoriaName, Descricao) " +
+                "VALUES ('Whisky', 'Bebidas alcoólicas destiladas, feitas a partir da fermentação de grãos como cevada, milho, centeio ou trigo, e envelhecidas em barris de madeira')");
+
+            migrationBuilder.Sql("INSERT INTO Categorias (CategoriaName, Descricao) " +
+                "VALUES ('Vinho', 'Bebidas alcoólicas obtidas pela fermentação do suco de uvas, apreciadas por seus diferentes aromas, sabores e estilos, que variam conforme a uva e o processo de produção.')");
+
+            migrationBuilder.Sql("INSERT INTO Categorias (CategoriaName, Descricao) " +
+                "VALUES ('Destilado', 'Bebidas alcoólicas produzidas pela destilação de líquidos fermentados, resultando em maior teor alcoólico e sabores concentrados')");
+
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias')");
+        }
+    }
+}
+
