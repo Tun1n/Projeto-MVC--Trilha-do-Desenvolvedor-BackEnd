80562ac282ce51c73fab54efe03b834589ae0d33
Tun1n <slpedro21@outlook.com>
2025-08-30 09:46:53 -0300

    feat: New TagHelper (EmailTagHelper) and implement Contato Controller

--- Solution1/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ Solution1/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd17bcf9c2fb260f4b7d2839d21e59c34ce4d921")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d14d80be38606fc308a3285bc7744d64a3a4358")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
