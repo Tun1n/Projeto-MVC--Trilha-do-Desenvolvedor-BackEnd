36bc497895c540d359a1df1ad94cf22a5cf3a006
Tun1n <slpedro21@outlook.com>
2025-08-31 16:30:51 -0300

    feat(Class Pedido and class PedidoDetalhe): Expanding the domain model and registering the mapping of these classes to tables in the database (DbSet)

--- Solution1/WebApplication1/Context/AppDbContext.cs
+++ Solution1/WebApplication1/Context/AppDbContext.cs
@@ -15,5 +15,8 @@ namespace WebApplication1.Context
         public DbSet<Categoria> Categorias { get; set; }
 
         public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }  
+
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalhe> PedidoDetalhes { get; set; }
     }
 }
--- Solution1/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ Solution1/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b234f39e43ff2826e72e86e2850af2dfe9c601b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b486f609fe01fbccc4b87a6fc16aebab5c717946")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
