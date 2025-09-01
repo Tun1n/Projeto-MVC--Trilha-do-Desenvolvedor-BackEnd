573fc858ea7fce535fb1efe8f10d1708ff075619
Tun1n <slpedro21@outlook.com>
2025-09-01 15:54:22 -0300

    Method HttpGet and HttpPost (Checkout) in PedidoController

--- Solution1/WebApplication1/Controllers/PedidoController.cs
+++ Solution1/WebApplication1/Controllers/PedidoController.cs
@@ -25,7 +25,38 @@ namespace WebApplication1.Controllers
         [HttpPost]
         public IActionResult Checkout(Pedido pedido)
         {
-            return View();
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0.0m;
+
+            List<CarrinhoCompraItem> items = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItems = items;
+
+            if(_carrinhoCompra.CarrinhoCompraItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho está vazio!!!");
+            }
+
+            foreach(var item in items)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Bebida.Preco * item.Quantidade);
+            }
+
+            // Valida os dados do pedido
+
+            if (ModelState.IsValid)
+            {
+                _pedidoRepository.CriarPedido(pedido);
+
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido :)";   
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                _carrinhoCompra.LimparCarrinho();
+
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+
+            return View(pedido);
         }
     }
 }
