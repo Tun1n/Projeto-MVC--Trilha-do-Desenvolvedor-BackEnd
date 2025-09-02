2818e623d4e5a2436decf3ac3fe5bf64d11637a4
Tun1n <slpedro21@outlook.com>
2025-09-02 10:52:23 -0300

    Implementation login

--- Solution1/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ Solution1/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bad30e20260c2bb170855641a6f56fa3f4982219")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a9ea2beb9c47fce3cb4102a9e09bec744956fc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
