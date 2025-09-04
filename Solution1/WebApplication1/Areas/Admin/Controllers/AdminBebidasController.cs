89e40e29fcc9fb04e5f2320273b9d2054ae34b07
Tun1n <slpedro21@outlook.com>
2025-09-04 11:30:32 -0300

    feat: CRUD (Create, read, update and delete): Fastest way to manage website orders, drinks and categories

---
+++ Solution1/WebApplication1/Areas/Admin/Controllers/AdminBebidasController.cs
@@ -0,0 +1,156 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Context;
+using WebApplication1.Models;
+
+namespace WebApplication1.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminBebidasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AdminBebidasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Bebidas.Include(l => l.Categoria);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bebida = await _context.Bebidas
+                .Include(l => l.Categoria)
+                .FirstOrDefaultAsync(m => m.BebidaId == id);
+            if (bebida == null)
+            {
+                return NotFound();
+            }
+
+            return View(bebida);
+        }
+
+        
+        public IActionResult Create()
+        {
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "CategoriaId", "CategoriaName");
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BebidaId,Name,DescricaoCurta,DescricaoDetalhada,Preco,ImagemUrl,ImagemThumbnailUrl,IsBebidaPreferida,EmEstoque,CategoriaId")] Bebida bebida )
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bebida);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "CategoriaId", "CategoriaName", bebida.CategoriaId);
+            return View(bebida);
+        }
+
+        
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bebida = await _context.Bebidas.FindAsync(id);
+            if (bebida == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "CategoriaId", "CategoriaName", bebida.CategoriaId);
+            return View(bebida);
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BebidaId,Name,DescricaoCurta,DescricaoDetalhada,Preco,ImagemUrl,ImagemThumbnailUrl,IsBebidaPreferida,EmEstoque,CategoriaId")] Bebida bebida)
+        {
+            if (id != bebida.BebidaId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bebida);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BebidaExists(bebida.BebidaId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "CategoriaId", "CategoriaName", bebida.CategoriaId);
+            return View(bebida);
+        }
+
+        
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bebida = await _context.Bebidas
+                .Include(l => l.Categoria)
+                .FirstOrDefaultAsync(m => m.BebidaId == id);
+            if (bebida == null)
+            {
+                return NotFound();
+            }
+
+            return View(bebida);
+        }
+
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var bebida = await _context.Bebidas.FindAsync(id);
+            _context.Bebidas.Remove(bebida);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BebidaExists(int id)
+        {
+            return _context.Bebidas.Any(e => e.BebidaId == id);
+        }
+    }
+}
--- Solution1/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ Solution1/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+915cb6ffab03018f21fc162cd8ac5d85d68cdaf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c1dbdfb84cfd3ce98a33227d02d35e59f480a01")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
